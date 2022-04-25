92e1bcd2a606f6be350509ec0d84ba12130f0711
OHHANSOO <zhdnrk124@naver.com>
2022-04-25 21:11:17 +0900

    #2022-04-25 addr : 봉천점 데일리스터디카페 time : 08:00 ~ 09:20 content : RestFull 중 Get

---
+++ src/main/java/com/example/hello/controller/GetApiController.java
@@ -0,0 +1,49 @@
+package com.example.hello.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/get")
+public class GetApiController {
+
+    @GetMapping(path = "/hello")
+    public String Hello(){
+
+        return "get Hello";
+    }
+
+    //get / post / put /delete 모두 걸림
+    @RequestMapping(path = "/allHi")
+    public String allHi(){
+        return "allHi";
+    }
+
+    //get http:localhost:9090/api/get/hi
+    @RequestMapping(path = "hi", method = RequestMethod.GET)
+    public String hi(){
+        return "hi";
+    }
+
+    //path에는 대소문자 구분이 안되기 떄문에 가독서을 위해 - 사용
+    // http:localhost:9090/api/get/path-variable/{name}
+    @GetMapping("/path-variable/{name}")
+    public String pathVariable(@PathVariable String name){
+        System.out.println("pathVariable : " + name);
+
+        return name;
+    }
+//변수명을 다르게 받아오고 싶을 땐 아래와 같이 사용
+/*    @GetMapping("/path-variable/{name}")
+    public String pathVariable(@PathVariable(name = "name") String Rname){
+        System.out.println("pathVariable : " + Rname);
+
+        return Rname;
+    }*/
+
+    //쿼리 파라미터 -> 주소창에 ?붙으면서 키와 value형태로 붙은 것
+
+    // http:localhost:9090/api/get/query-param?user=steve&mail=steve@name
+
+
+}
